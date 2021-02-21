9afd91623d6f0e8e17e72a5248f44753da0c6fed
HamaguchiKazuki <haganai1998@gmail.com>
2021-02-21 17:54:50 +0900

    (fix) method to class of download file

---
+++ src/downloader.py
@@ -0,0 +1,39 @@
+#
+# Created on Mon Feb 15 2021
+#
+# Copyright (c) 2021 Kazuki Hamaguchi
+#
+
+import os
+import time
+import requests
+from requests.models import ITER_CHUNK_SIZE
+
+
+class Downloader():
+
+    def get_file(self, url_path, save_dir, iter_chunk_size=ITER_CHUNK_SIZE):
+        """download a file.
+
+        Args:
+            url_path ([type]): [description]
+            save_dir ([type]): [description]
+            iter_chunk_size ([type], optional): [description]. Defaults to ITER_CHUNK_SIZE.
+
+        Returns:
+            [err]: error handling, None is OK.
+        """
+        err = None
+        os.makedirs(save_dir, exist_ok=True)
+        res = requests.get(url_path)
+        time.sleep(1.0)
+        try:
+            res.raise_for_status()
+        except Exception as err:
+            return err
+        pdf_name = os.path.basename(url_path)
+        pdf_save_path = os.path.join(save_dir, pdf_name)
+        with open(pdf_save_path, "wb") as pdf_data:
+            for chunk_pdf_data in res.iter_content(iter_chunk_size):
+                pdf_data.write(chunk_pdf_data)
+        return err
--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -5,19 +5,21 @@
 #
 
 import unittest
-from src.download_file import download_file
+from src.downloader import Downloader
 import os
 
 TEST_FILE_URL = "https://raw.githubusercontent.com/HamaguchiKazuki/download_paper/main/tests/example.com/content/papers/sample.txt"
+
 
 class TestDownloadFile(unittest.TestCase):
 
     def test_download_file(self):
         test_url_path = TEST_FILE_URL
         test_dir = "tests/test_dir"
-        err = download_file(test_url_path, test_dir)
+        d = Downloader()
+        err = d.get_file(test_url_path, test_dir)
         if not err == None:
-            self.assertFalse(err) 
+            self.assertFalse(err)
         test_file_path = os.path.join(
             test_dir, os.path.basename(test_url_path))
         with open(test_file_path, "r") as f:
