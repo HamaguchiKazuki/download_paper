05592467c44eb41feaff7eb721ea78fffc4020be
HamaguchiKazuki <haganai1998@gmail.com>
2021-02-21 17:23:53 +0900

    (add) test case out of range year

--- tests/test_cvf_searcher.py
+++ tests/test_cvf_searcher.py
@@ -40,3 +40,21 @@ class TestCvfSearcher(unittest.TestCase):
         checked_paper_list = [
             paper for paper in paper_url_list if ".pdf" in paper]
         self.assertListEqual(checked_paper_list, paper_url_list)
+
+    def test_out_of_range_year(self):
+        cvpr_sercher = CvfSearcher(conference_name="CVPR")
+        wacv_sercher = CvfSearcher(conference_name="WACV")
+        iccv_sercher = CvfSearcher(conference_name="ICCV")
+        out_of_range_year = ["1800"]
+        _, cvpr_err = cvpr_sercher.find_paper_url_list(out_of_range_year)
+        _, wacv_err = wacv_sercher.find_paper_url_list(out_of_range_year)
+        _, iccv_err = iccv_sercher.find_paper_url_list(out_of_range_year)
+        is_exist_cvpr_err = (not cvpr_err == None)
+        is_exist_wacv_err = (not wacv_err == None)
+        is_exist_iccv_err = (not iccv_err == None)
+        if all([is_exist_cvpr_err, is_exist_wacv_err, is_exist_iccv_err]):
+            self.assertTrue(
+                f"is_exist_cvpr_err, is_exist_wacv_err, is_exist_iccv_err,{[is_exist_cvpr_err, is_exist_wacv_err, is_exist_iccv_err]}")
+        else:
+            self.assertFalse(
+                f"allow out of range year. is_exist_cvpr_err, is_exist_wacv_err, is_exist_iccv_err,{[is_exist_cvpr_err, is_exist_wacv_err, is_exist_iccv_err]}")
