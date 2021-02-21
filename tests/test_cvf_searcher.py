c9b82c4bfe7383efb1a69797b9582991cf8f1c63
hama-matcha <hamamatcha@gmail.com>
2021-02-21 16:47:26 +0900

    (add) searcher CVF papers

--- tests/test_cvf_searcher.py
+++ tests/test_cvf_searcher.py
@@ -10,7 +10,7 @@ from src.searcher import CvfSearcher
 class TestCvfSearcher(unittest.TestCase):
 
     def test_cvpr_searcher(self):
-        cvpr_sercher = CvfSearcher(conference_journal_name="CVPR")
+        cvpr_sercher = CvfSearcher(conference_name="CVPR")
         year_list = ["2013", "2014", "2015", "2016", "2017", "2018", "2019",  "2020"]
         paper_url_list, err = cvpr_sercher.find_paper_url_list(year_list)
         if not err == None:
@@ -20,7 +20,7 @@ class TestCvfSearcher(unittest.TestCase):
         self.assertListEqual(checked_paper_list, paper_url_list)
 
     def test_wacv_searcher(self):
-        wacv_sercher = CvfSearcher(conference_journal_name="WACV")
+        wacv_sercher = CvfSearcher(conference_name="WACV")
         year_list = ["2020", "2021"]
         paper_url_list, err = wacv_sercher.find_paper_url_list(year_list)
         if not err == None:
@@ -31,7 +31,7 @@ class TestCvfSearcher(unittest.TestCase):
 
 
     def test_iccv_searcher(self):
-        iccv_sercher = CvfSearcher(conference_journal_name="ICCV")
+        iccv_sercher = CvfSearcher(conference_name="ICCV")
         year_list = ["2013", "2015", "2017", "2019"]
         paper_url_list, err = iccv_sercher.find_paper_url_list(year_list)
         if not err == None:
